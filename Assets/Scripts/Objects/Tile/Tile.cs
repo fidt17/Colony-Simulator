e8cc5af98153c281f943469c12786f035c56e5b3
fidt17 <fidt17@gmail.com>
2020-10-16 15:13:44 +0300

    Clean Tile class. .gitignore test

--- Assets/Scripts/Objects/Tile/Tile.cs
+++ Assets/Scripts/Objects/Tile/Tile.cs
@@ -12,39 +12,15 @@ public enum TileType {
 
 public class Tile : IData {
 
-    private class TileHighlight {
-
-        public Color Color => _color;
-
-        private Color _color = Color.white;
-        private Color _defaultColor = Color.white;
-        private Vector2Int _position;
-
-        public TileHighlight(Vector2Int position, Color defaultColor) {
-            _position = position;
-            _defaultColor = defaultColor;
-        }
-
-        public void SetColor(Color color) {
-            _color = color;
-            MeshGenerator.GetInstance().UpdateChunkAt(_position.x, _position.y);
-        }
-
-        public void ResetColor() {
-            _color = _defaultColor;
-            MeshGenerator.GetInstance().UpdateChunkAt(_position.x, _position.y);
-        }
-    }
-
     public TileType type { get; set; }
     public TileContent content { get; private set; }
 
     public TileScriptableObject data { get; protected set; }
+    public Sprite GetSprite() => data.prefabSprite;
+    public Color GetColor() => data.defaultColor;
 
     public Vector2Int position   { get; protected set; }
     public bool isTraversable    { get; protected set; }
-
-    private TileHighlight _tileHighlight;
 
     public void SetData(PrefabScriptableObject data, Vector2Int position) {
         this.data = data as TileScriptableObject;
@@ -53,7 +29,6 @@ public class Tile : IData {
         this.position = position;
 
         content = new TileContent(this);
-        _tileHighlight = new TileHighlight(this.position, this.data.defaultColor);
     }
 
     public void SetTraversability(bool isTraversable) {
@@ -68,9 +43,4 @@ public class Tile : IData {
             Pathfinder.UpdateSystemAt(position.x, position.y);
         }
     }
-
-    public Sprite GetSprite() => data.prefabSprite;
-    public Color GetColor() => _tileHighlight.Color;
-    public void SetColor(Color color) => _tileHighlight.SetColor(color);
-    public void ResetColor() => _tileHighlight.ResetColor();
 }
